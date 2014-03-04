268c4dce6cfa59e10cff7f4bf8456276c2e11f7d
Dimitri Molenaars <Tyrope@TyRope.nl>
2014-03-04 12:16:58 +0100

    Create config paths if needed, find files and (for now) report remotes.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ KSP Add-on Version Checker.
 # Copyright 2014 Dimitri "Tyrope" Molenaars
 
 # Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use self file except in compliance with the License.
+# you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 
 #    http://www.apache.org/licenses/LICENSE-2.0
@@ -18,20 +18,41 @@ KSP Add-on Version Checker.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from config import Config
-import os
-import versionComparator
+import config
+import os, sys
+import versionComparator as verComp
 
 def main():
-    cf = os.path.join(os.path.expanduser('~'), '.KSP-AVC','default.cfg')
-    cfg = Config(cf)
-    print cfg.get('install_dir')
+    # Find config folder.
+    cfg_dir = os.path.join(os.path.expanduser('~'), '.KSP-AVC')
+    # Create it if needed.
+    if not os.path.exists(cfg_dir):
+        os.makedirs(cfg_dir)
 
+    # Create config object.
+    cfg = config.Config(os.path.join(cfg_dir, 'default.cfg'))
+
+    for mod in findMods(cfg):
+        remote = verComp.getRemote(mod)
+        print "Found version file %s reporting remote %s" % (mod, remote)
+        # comp = verComp.versionComparator(mod, remote)
 
     #Shutdown procedure
-    if cfg.need_save():
-        cfg.save()
+    cfg.save()
     sys.exit(0)
 
+def findMods(cfg):
+    mods = set()
+    # Walk through the directories
+    for path, folders, files in os.walk(cfg.get('gamedata_dir')):
+        # Walk through the files.
+        for f in files:
+            # Found a version file.
+            if f.lower().endswith(".version"):
+                mods.add(os.path.join(path, f))
+    return mods
+
+# Startup sequence
 if __name__ == '__main__':
     main()
+
